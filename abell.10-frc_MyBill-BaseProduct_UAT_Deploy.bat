60e71141305f10812132454736ef7989967c1a01
nathan.tran <nathan.tran@anacle.com>
2025-10-29 11:12:15 +0800

    Validate if solution dir exists

--- abell.10-frc_MyBill-BaseProduct_UAT_Deploy.bat
+++ abell.10-frc_MyBill-BaseProduct_UAT_Deploy.bat
@@ -14,6 +14,12 @@ echo SolutionDirectory: %SolutionDirectory% >> %LogFilePath%
 echo DestinationDirectory: %DestinationDirectory% >> %LogFilePath%
 echo LogFilePath: %LogFilePath% >> %LogFilePath%
 
+rem Check if solution directory exists
+if not exist "%SolutionDirectory%" (
+	echo ERROR: Solution directory does not exist: %SolutionDirectory%    >> %LogFilePath% 2>&1
+	goto CompleteWithError
+)
+
 rem 2. Create destination directory structure.
 echo ^<---- Create folders ----^> >> %LogFilePath%
 
