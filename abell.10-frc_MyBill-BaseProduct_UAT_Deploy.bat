c30886c39a134ed4b31af2da5e480da8f9b62f08
nathan.tran <nathan.tran@anacle.com>
2025-10-29 11:06:50 +0800

    Fix bugs and refactor local deployment script

--- abell.10-frc_MyBill-BaseProduct_UAT_Deploy.bat
+++ abell.10-frc_MyBill-BaseProduct_UAT_Deploy.bat
@@ -1,39 +1,20 @@
 rem @echo off
-rem for /F "tokens=2-4 delims=/- " %%A in ('date/T') do set DATE=%%C%%A%%B
-for /f "skip=1" %%x in ('wmic os get localdatetime') do if not defined MyDATE set MyDATE=%%x
-set year=%MyDATE:~0,4%
-set month=%MyDATE:~4,2%
-set day=%MyDATE:~6,2%
-set datetrf=%year%%month%%day%
+rem Get start date/time
+for /f %%a in ('powershell -Command "Get-Date -Format 'yyyyMMdd'"') do set datetrf=%%a
 
-rem Day of week
-rem Mon,	Tue,		Wed,		Thu,		Fri,		Sat,		Sun
-rem 1,		2,		3,		4,		5,		6,		0
-for /F "tokens=2 skip=2 delims=," %%D in ('WMIC Path Win32_LocalTime Get DayOfWeek /Format:csv') do set dow=%%D
-
-rem space in path is allowed 
-set "SourceSolutionSLNDirectory=C:\Anacle\SP\simplicity\abell.root\abell"
+rem 1. Define source and destination directories. Space in path is allowed. 
+set "SolutionDirectory=C:\Anacle\SP\simplicity\abell.root\abell"
 set "DestinationDirectory=D:\deployment\SP\UAT_%datetrf%\"
 set "LogFilePath=D:\deployment\SP\UAT_%datetrf%.log"
 
+for /f %%a in ('powershell -Command "Get-Date -Format 'yyyyMMddHHmmss'"') do set MyDATE=%%a
 echo ^<---- Start at %MyDATE% ----^> >> %LogFilePath%
 
-echo SourceSolutionSLNDirectory: %SourceSolutionSLNDirectory% >> %LogFilePath%
+echo SolutionDirectory: %SolutionDirectory% >> %LogFilePath%
 echo DestinationDirectory: %DestinationDirectory% >> %LogFilePath%
 echo LogFilePath: %LogFilePath% >> %LogFilePath%
 
-rem call AdjustDate.bat -1 > previousDate.txt   
-rem set /p PreviousDate= < previousDate.txt 
-rem call AdjustDate.bat +0 > currentDate.txt   
-rem set /p CurrentDate= < currentDate.txt 
-rem call AdjustDate.bat +1 > nextDate.txt   
-rem set /p NextDate= < nextDate.txt 
-rem 
-rem echo Previous Date: %PreviousDate%  >> %LogFilePath%
-rem echo Current Date: %CurrentDate%  >> %LogFilePath%
-rem echo Next Date: %NextDate%  >> %LogFilePath%
-
-rem 
+rem 2. Create destination directory structure.
 echo ^<---- Create folders ----^> >> %LogFilePath%
 
 if exist "%DestinationDirectory%" (
@@ -43,28 +24,21 @@ if exist "%DestinationDirectory%" (
 
 call:CreateDirectory "%DestinationDirectory%"
 
-rem open destination folder 
+rem Open destination folder and create subfolders
 cd "%DestinationDirectory%"
-start .
-
 
 call:CreateDirectory "%DestinationDirectory%\webapp\"
 call:CreateDirectory "%DestinationDirectory%\service\"
 call:CreateDirectory "%DestinationDirectory%\TPAPI\"
-rem call:CreateDirectory "%DestinationDirectory%\WebServiceToCRM\"
-rem call:CreateDirectory "%DestinationDirectory%\WebServiceToEBT\"
-rem call:CreateDirectory "%DestinationDirectory%\WebServiceToMSSL\"
 call:CreateDirectory "%DestinationDirectory%\ConfigFiles\"
 
+rem 3. Copy files from source to destination.
 echo ^<---- Copy files ----^> >> %LogFilePath%
-xcopy "%SourceSolutionSLNDirectory%\webapp\." "%DestinationDirectory%\webapp\" 	/e	/f				  >> %LogFilePath% 2>&1
-xcopy "%SourceSolutionSLNDirectory%\service\bin\debug\." "%DestinationDirectory%\service\"  /e /f          >> %LogFilePath% 2>&1
-xcopy "%SourceSolutionSLNDirectory%\AnacleAPI.Interface\bin\app.publish\." "%DestinationDirectory%\TPAPI\"  /e /f      >> %LogFilePath% 2>&1
-rem xcopy "%SourceSolutionSLNDirectory%\WebServiceToCRM\." "%DestinationDirectory%\WebServiceToCRM\" /e /f     >> %LogFilePath% 2>&1
-rem xcopy "%SourceSolutionSLNDirectory%\WebServiceToEBT\." "%DestinationDirectory%\WebServiceToEBT\"  /e /f    >> %LogFilePath% 2>&1
-rem xcopy "%SourceSolutionSLNDirectory%\WebServiceToMSSL\." "%DestinationDirectory%\WebServiceToMSSL\" /e /f   >> %LogFilePath% 2>&1
+xcopy "%SolutionDirectory%\webapp\." "%DestinationDirectory%\webapp\" 	/e	/f				  					>> %LogFilePath% 2>&1
+xcopy "%SolutionDirectory%\service\bin\debug\." "%DestinationDirectory%\service\"  /e /f          				>> %LogFilePath% 2>&1
+xcopy "%SolutionDirectory%\AnacleAPI.Interface\bin\app.publish\." "%DestinationDirectory%\TPAPI\"  /e /f      	>> %LogFilePath% 2>&1
 
-rem delete config files
+rem 4. Delete config files (Move to ConfigFiles folder).
 echo ^<---- Delete config files ----^> >> %LogFilePath%
 call:DeleteConfigFile "%DestinationDirectory%\webapp\web.config"
 call:DeleteConfigFile "%DestinationDirectory%\webapp\web.config.bak"
@@ -77,42 +51,21 @@ call:DeleteConfigFile "%DestinationDirectory%\service\LogicLayer.dll.config"
 
 call:DeleteConfigFile "%DestinationDirectory%\TPAPI\Web.config"
 
-rem call:DeleteConfigFile "%DestinationDirectory%\WebServiceToCRM\WebServiceToCRM.csproj"
-rem call:DeleteConfigFile "%DestinationDirectory%\WebServiceToCRM\WebServiceToCRM.csproj.user"
-rem call:DeleteConfigFile "%DestinationDirectory%\WebServiceToCRM\Web.config"
-rem call:DeleteConfigFile "%DestinationDirectory%\WebServiceToCRM\Web.Debug.config"
-rem call:DeleteConfigFile "%DestinationDirectory%\WebServiceToCRM\Web.Release.config"
-
-rem call:DeleteConfigFile "%DestinationDirectory%\WebServiceToEBT\WebServiceToEBT.csproj"
-rem call:DeleteConfigFile "%DestinationDirectory%\WebServiceToEBT\WebServiceToEBT.csproj.user"
-rem call:DeleteConfigFile "%DestinationDirectory%\WebServiceToEBT\Web.config"
-rem call:DeleteConfigFile "%DestinationDirectory%\WebServiceToEBT\Web.Debug.config"
-rem call:DeleteConfigFile "%DestinationDirectory%\WebServiceToEBT\Web.Release.config"
-
-rem call:DeleteConfigFile "%DestinationDirectory%\WebServiceToMSSL\WebServiceToMSSL.csproj"
-rem call:DeleteConfigFile "%DestinationDirectory%\WebServiceToMSSL\WebServiceToMSSL.csproj.user"
-rem call:DeleteConfigFile "%DestinationDirectory%\WebServiceToMSSL\Web.config"
-rem call:DeleteConfigFile "%DestinationDirectory%\WebServiceToMSSL\Web.Debug.config"
-rem call:DeleteConfigFile "%DestinationDirectory%\WebServiceToMSSL\Web.Release.config"
-
+rem 5. Complete
 :Complete
-for /f "skip=1" %%x in ('wmic os get localdatetime') do if not defined EndDATE set EndDATE=%%x
+for /f %%a in ('powershell -Command "Get-Date -Format 'yyyyMMdd_HHmmss'"') do set EndDATE=%%a
 echo ^<---- Complete at %EndDATE% ----^> >> %LogFilePath%
 echo .>> %LogFilePath%
-rem open log file 
 start notepad "%LogFilePath%"
-
+start "" "%DestinationDirectory%"
 exit
 
+
 :CompleteWithError
-for /f "skip=1" %%x in ('wmic os get localdatetime') do if not defined EndDATE set EndDATE=%%x
+for /f %%a in ('powershell -Command "Get-Date -Format 'yyyyMMdd_HHmmss'"') do set EndDATE=%%a
 echo ^<---- Complete With Error at %EndDATE% ----^> >> %LogFilePath%
 echo .>> %LogFilePath%
-rem open log file 
 start notepad "%LogFilePath%"
-cd %DestinationDirectory%
-start .
-
 exit
 
 :CreateDirectory
@@ -138,6 +91,3 @@ if exist "%tempConfigFileToDelete%" (
 	echo %tempConfigFileToDelete% is not found 	>> %LogFilePath% 2>&1
 )
 goto :eof
-
-
-
